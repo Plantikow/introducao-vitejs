96162cb625042954a6ec434540f558abe674b0cc
Plantikow <kicoplantikow@gmail.com>
2026-03-10 10:55:02 -0300

    Aula 10 anything

---
+++ src/pages/exemplos/ex-01/index.jsx
@@ -0,0 +1,10 @@
+function Exemplo01() {
+    return (
+        <>
+           <h1> exemplo 1</h1>
+           <p>COmponente basico para renderizar tela com react</p>
+        </>
+    )
+}
+
+export default Exemplo01;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return(
+        <>
+            <h1>Aula react com vite js</h1>
+        </>
+    )
+}
+
+export default Home;
